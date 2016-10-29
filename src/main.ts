596e8f03a492199491bc36fe174884316a8b7d93
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-29 22:29:33 +0100

    Arrow function is more succint.

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,7 @@ class FRP
         const a: Cell<number> = textA.text.map(s => parseInt(s));
         const b: Cell<number> = textB.text.map(s => parseInt(s));
 
-        const sum: Cell<number> = a.lift(b, (a_, b_) =>
-        {
-            return a_ + b_;
-        });
+        const sum: Cell<number> = a.lift(b, (a_, b_) => a_ + b_);
 
         const lblSum: SLabel = new SLabel(sum.map(i => i.toString()));
     }
