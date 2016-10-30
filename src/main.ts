10bc187b03261093efcbf48f841aa95cec09bf12
Maciej Sitko <maciej@neatcube.co.uk>
2016-10-30 15:31:23 +0000

    SSpinner dependency (unforuntate)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import STextField from './stext-field';
-import Label from './label';
-
 import Arr from './Arr';
+
+import STextField from './stext-field';
+import SSpinner from './sspinner';
+import Label from './label';
 
 import {transactionally, Cell} from 'sodiumjs';
 
@@ -11,12 +12,12 @@ class FRP
 
     public static main()
     {
-        const labels: Array<Label> = new Array<Label>();
-        const fields: Array<STextField> = new Array<STextField>();
+        const labels: Array<Label> = new Array<Label>(FRP.maxEmails + 2);
+        const fields: Array<STextField> = new Array<STextField>(FRP.maxEmails + 2);
 
         const valids: Array<Cell<string>> = Arr.newInstance<Cell<string>>(new Cell<string>(""), FRP.maxEmails + 2);
 
-        const row: number = 0;
+        var row: number = 0;
 
         const name: STextField = new STextField("");
 
@@ -26,7 +27,18 @@ class FRP
                 new String(t).trim() === "" ? "<-- enter something" :
                 new String(t).trim().indexOf(' ') < 0 ? "<-- must contain space" : "");
 
-
+        row += 1;
+        /*
+        const emails: Array<STextField> =  new Array<STextField>(FRP.maxEmails);
+        for(let i = 0; i < FRP.maxEmails; i++, row++)
+        {
+            labels[row] = new Label(`Email # ${i + 1}`);
+            const ii: number = i;
+            const email: STextField = new STextField("");
+            fields[row] = email;
+            valids[row] = email.text.lift(2,)
+        }
+        */
     }
 }
 
